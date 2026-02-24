de741a651a4b761de936d4b75b09ac05535b3b97
NishiOwO <nishi@nishi.boats>
2026-02-24 18:46:17 +0900

    fix

--- axe/axeproject.pas
+++ axe/axeproject.pas
@@ -1,7 +1,7 @@
 unit AxeProject;
 
 interface
-function AxeProjectGet(ID : String; DT : String; Token: String) : boolean;
+function AxeProjectGet(ID : String; DT : String; Token: String) : Integer;
 procedure AxeProjectGet(ID : String);
 
 implementation
@@ -72,28 +72,43 @@ begin
 	ProjectTargetIteration(ID, JTarget, 'sounds');
 end;
 
-function AxeProjectGet(ID : String; DT : String; Token: String) : boolean;
+function AxeProjectGet(ID : String; DT : String; Token: String) : Integer;
 var
 	JStr : String;
 	JData, JTargets, JTarget : TJSONData;
 	ProjectJSON : TextFile;
 	I : Integer;
 begin
-	AxeProjectGet := true;
+	AxeProjectGet := 1;
 	while true do
 	begin
 		try JStr := TFPHTTPClient.SimpleGet('https://projects.scratch.mit.edu/' + ID + '?token=' + Token);
 		except
-			WriteLn('https://projects.scratch.mit.edu/' + ID + '?token=' + Token);
-			WriteLn(StdErr, '[' + ID + '] Failed to get project.json - retrying');
-			continue;
+			on E : EHTTPClient do
+			begin
+				if E.StatusCode = 403 then
+				begin
+					WriteLn(StdErr, '[' + ID + '] Failed to get project.json - getting token again...');
+					AxeProjectGet := 2;
+					Exit;
+				end
+				else
+				begin
+					WriteLn(StdErr, '[' + ID + '] Failed to get project.json - retrying');
+					continue;
+				end;
+			end;
+			on E : Exception do
+			begin
+				continue;
+			end;
 		end;
 		break;
 	end;
 	try
 		JData := GetJSON(JStr, false);
 	except
-		AxeProjectGet := false;
+		AxeProjectGet := 0;
 		Exit;
 	end;
 
@@ -134,146 +149,158 @@ var
 	Skip : Boolean;
 	Entry : TAxeDatabaseEntry;
 	N : Integer;
+	RetValue : Integer;
 begin
-	while true do
-	begin
-		try JStr := TFPHTTPClient.SimpleGet('https://api.scratch.mit.edu/projects/' + ID);
-		except
-			WriteLn(StdErr, '[' + ID + '] Failed to get project token - retrying');
-			continue;
-		end;
-		break;
-	end;
-	JData := GetJSON(JStr, false);
-
 	TryISOStrToDateTime('2026-01-22T00:00Z', BadDT);
 
-	JToken := JData.FindPath('project_token');
-	if Assigned(JToken) then
+	while true do
 	begin
-		JDate := JData.FindPath('history.modified');
-		if Assigned(JDate) then
+		while true do
 		begin
-			TryISOStrToDateTime(JDate.AsString, DT);
-			if CompareDateTime(DT, BadDT) <= 0 then
+			try JStr := TFPHTTPClient.SimpleGet('https://api.scratch.mit.edu/projects/' + ID);
+			except
+				WriteLn(StdErr, '[' + ID + '] Failed to get project token - retrying');
+				continue;
+			end;
+			break;
+		end;
+		JData := GetJSON(JStr, false);
+	
+		JToken := JData.FindPath('project_token');
+		if Assigned(JToken) then
+		begin
+			JDate := JData.FindPath('history.modified');
+			if Assigned(JDate) then
 			begin
-				CreateDir('projects');
-				CreateDir('projects/' + ID);
-				CreateDir('projects/' + ID + '/' + JDate.AsString);
-
-				JObj := JData as TJSONObject;
-
-				N := 0;
-				if Assigned(JObj.FindPath('id')) then
+				TryISOStrToDateTime(JDate.AsString, DT);
+				if CompareDateTime(DT, BadDT) <= 0 then
 				begin
-					Entry.ProjectID := JObj.Integers['id'];
-					N := N + 1;
-				end;
-				if Assigned(JObj.FindPath('title')) then
-				begin
-					Entry.Title := JObj.Strings['title'];
-					N := N + 1;
-				end;
-				if Assigned(JObj.FindPath('description')) then
-				begin
-					Entry.Description := JObj.Strings['description'];
-					N := N + 1;
-				end;
-				if Assigned(JObj.FindPath('instructions')) then
-				begin
-					Entry.Instructions := JObj.Strings['instructions'];
-					N := N + 1;
-				end;
-				if Assigned(JObj.FindPath('author.id')) then
-				begin
-					Entry.AuthorID := JObj.Objects['author'].Integers['id'];
-					N := N + 1;
-				end;
-				if Assigned(JObj.FindPath('author.username')) then
-				begin
-					Entry.AuthorName := JObj.Objects['author'].Strings['username'];
-					N := N + 1;
-				end;
-				if Assigned(JObj.FindPath('history.modified')) then
-				begin
-					Entry.Timestamp := JObj.Objects['history'].Strings['modified'];
-					N := N + 1;
-				end;
-
-				Skip := false;
-
-				if FileExists('projects/' + ID + '/' + JDate.AsString + '/metadata.json') then
-				begin
-					FS := TFileStream.Create('projects/' + ID + '/' + JDate.AsString + '/metadata.json', fmOpenRead);
-
-					JMeta := GetJSON(FS);
-					JMetaFound := JMeta.FindPath('notFound');
-
-					Skip := true;
-					if Assigned(JMetaFound) then
+					CreateDir('projects');
+					CreateDir('projects/' + ID);
+					CreateDir('projects/' + ID + '/' + JDate.AsString);
+	
+					JObj := JData as TJSONObject;
+	
+					N := 0;
+					if Assigned(JObj.FindPath('id')) then
 					begin
-						if not(JMetaFound.AsBoolean) then Skip := false;
+						Entry.ProjectID := JObj.Integers['id'];
+						N := N + 1;
 					end;
-
-					if Skip then
+					if Assigned(JObj.FindPath('title')) then
+					begin
+						Entry.Title := JObj.Strings['title'];
+						N := N + 1;
+					end;
+					if Assigned(JObj.FindPath('description')) then
+					begin
+						Entry.Description := JObj.Strings['description'];
+						N := N + 1;
+					end;
+					if Assigned(JObj.FindPath('instructions')) then
+					begin
+						Entry.Instructions := JObj.Strings['instructions'];
+						N := N + 1;
+					end;
+					if Assigned(JObj.FindPath('author.id')) then
+					begin
+						Entry.AuthorID := JObj.Objects['author'].Integers['id'];
+						N := N + 1;
+					end;
+					if Assigned(JObj.FindPath('author.username')) then
+					begin
+						Entry.AuthorName := JObj.Objects['author'].Strings['username'];
+						N := N + 1;
+					end;
+					if Assigned(JObj.FindPath('history.modified')) then
+					begin
+						Entry.Timestamp := JObj.Objects['history'].Strings['modified'];
+						N := N + 1;
+					end;
+	
+					Skip := false;
+	
+					if FileExists('projects/' + ID + '/' + JDate.AsString + '/metadata.json') then
+					begin
+						FS := TFileStream.Create('projects/' + ID + '/' + JDate.AsString + '/metadata.json', fmOpenRead);
+	
+						JMeta := GetJSON(FS);
+						JMetaFound := JMeta.FindPath('notFound');
+	
+						Skip := true;
+						if Assigned(JMetaFound) then
+						begin
+							if not(JMetaFound.AsBoolean) then Skip := false;
+						end;
+	
+						if Skip then
+						begin
+							if N = 7 then
+							begin
+								AxeDatabaseAdd(Entry, False);
+							end;
+							WriteLn(StdErr, '[' + ID + '] Project has been scraped already - ignoring');
+						end;
+							
+						FS.Free();
+					end;
+	
+					if not(Skip) then
 					begin
 						if N = 7 then
 						begin
-							AxeDatabaseAdd(Entry, False);
+							AxeDatabaseAdd(Entry, True);
 						end;
-						WriteLn(StdErr, '[' + ID + '] Project has been scraped already - ignoring');
+	
+						AssignFile(MetaJSON, 'projects/' + ID + '/' + JDate.AsString + '/info.json');
+						Rewrite(MetaJSON);
+						Write(MetaJSON, JStr);
+						CloseFile(MetaJSON);
+	
+						WriteLn(StdErr, '[' + ID + '] Got project token');
+						RetValue := AxeProjectGet(ID, JDate.AsString, JToken.AsString);
+						if RetValue = 1 then
+						begin
+							JMeta := GetJSON('{}');
+							JObj := JMeta as TJSONObject;
+							JObj.Add('scrapedAt', DateToISO8601(Now()));
+	
+							AssignFile(InfoJSON, 'projects/' + ID + '/' + JDate.AsString + '/metadata.json');
+							Rewrite(InfoJSON);
+							Write(InfoJSON, JObj.AsJSON);
+							CloseFile(InfoJSON);
+						end
+						else if RetValue = 0 then
+						begin
+							JMeta := GetJSON('{}');
+							JObj := JMeta as TJSONObject;
+							JObj.Add('notFound', true);
+	
+							AssignFile(InfoJSON, 'projects/' + ID + '/' + JDate.AsString + '/metadata.json');
+							Rewrite(InfoJSON);
+							Write(InfoJSON, JObj.AsJSON);
+							CloseFile(InfoJSON);
+						end
+						else if RetValue = 2 then
+						begin
+							JMeta.Free();
+							JData.Free();
+							continue;
+						end;
+	
+						JMeta.Free();
 					end;
-						
-					FS.Free();
-				end;
-
-				if not(Skip) then
+				end
+				else
 				begin
-					if N = 7 then
-					begin
-						AxeDatabaseAdd(Entry, True);
-					end;
-
-					AssignFile(MetaJSON, 'projects/' + ID + '/' + JDate.AsString + '/info.json');
-					Rewrite(MetaJSON);
-					Write(MetaJSON, JStr);
-					CloseFile(MetaJSON);
-
-					WriteLn(StdErr, '[' + ID + '] Got project token');
-					if AxeProjectGet(ID, JDate.AsString, JToken.AsString) then
-					begin
-						JMeta := GetJSON('{}');
-						JObj := JMeta as TJSONObject;
-						JObj.Add('scrapedAt', DateToISO8601(Now()));
-
-						AssignFile(InfoJSON, 'projects/' + ID + '/' + JDate.AsString + '/metadata.json');
-						Rewrite(InfoJSON);
-						Write(InfoJSON, JObj.AsJSON);
-						CloseFile(InfoJSON);
-					end
-					else
-					begin
-						JMeta := GetJSON('{}');
-						JObj := JMeta as TJSONObject;
-						JObj.Add('notFound', true);
-
-						AssignFile(InfoJSON, 'projects/' + ID + '/' + JDate.AsString + '/metadata.json');
-						Rewrite(InfoJSON);
-						Write(InfoJSON, JObj.AsJSON);
-						CloseFile(InfoJSON);
-					end;
-
-					JMeta.Free();
+					WriteLn(StdErr, '[' + ID + '] Project is too new - ignoring');
 				end;
-			end
-			else
-			begin
-				WriteLn(StdErr, '[' + ID + '] Project is too new - ignoring');
 			end;
 		end;
+		
+		JData.Free();
+		break;
 	end;
-	
-	JData.Free();
 end;
 
 end.
